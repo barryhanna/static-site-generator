627a7c1b8e49e890899367d280a6a1939077f47f
Barry Hanna <barryahanna@gmail.com>
2024-04-18 09:09:17 +0100

    text_node_to_html_node

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,10 @@
+from htmlnode import HTMLNode
+from leafnode import LeafNode
+
+
 class TextNode():
+    text_types = ["text", "bold", "italic", "code", "link", "image"]
+
     def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
@@ -9,3 +15,19 @@ class TextNode():
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+    def text_node_to_html_node(self, text_node):
+        if not (text_node.text_type in self.text_types):
+            raise Exception("TextNode does not have a supported text type")
+        if (text_node.text_type == "text"):
+            return LeafNode(None, text_node.value)
+        if (text_node.text_type == "bold"):
+            return LeafNode("b", text_node.value)
+        if (text_node.text_type == "italic"):
+            return HTMLNode("i", text_node.value)
+        if (text_node.text_type == "code"):
+            return HTMLNode("code", text_node.value)
+        if (text_node.text_type == "link"):
+            return HTMLNode("a", text_node.value, {"href": text_node.props.href})
+        if (text_node.text_type == "image"):
+            return HTMLNode("img", "", {"src": text_node.url, "alt": text_node.value})
