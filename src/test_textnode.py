36e8f523a764c879948a34f19478c5d91c3be1dd
Barry Hanna <barryahanna@gmail.com>
2024-04-18 20:47:08 +0100

    test_split_nodes_delimiter more unit tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -18,13 +18,36 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode('This is a text node, but different', 'bold')
         self.assertNotEqual(node, node2)
 
-    def test_split_nodes_delimiter(self):
+    def test_split_nodes_delimiter_code(self):
         node = TextNode(
             "This is text with a `code block` word", "code")
         actual = TextNode.split_nodes_delimiter([node], "`", node.text_type)
         expected = [
             TextNode("This is text with a ", TextNode.text_type_text),
             TextNode("code block", TextNode.text_type_code),
+            TextNode(" word", TextNode.text_type_text)
+        ]
+        self.assertEqual(actual, expected)
+
+    def test_split_nodes_delimiter_bold(self):
+        node = TextNode(
+            "This is text with a **bold block** word", "bold")
+        actual = TextNode.split_nodes_delimiter(
+            [node], "**", node.text_type_bold)
+        expected = [
+            TextNode("This is text with a ", TextNode.text_type_text),
+            TextNode("bold block", TextNode.text_type_bold),
+            TextNode(" word", TextNode.text_type_text)
+        ]
+        self.assertEqual(actual, expected)
+
+    def test_split_nodes_delimiter_italic(self):
+        node = TextNode(
+            "This is text with a *italic block* word", "italic")
+        actual = TextNode.split_nodes_delimiter([node], "*", node.text_type)
+        expected = [
+            TextNode("This is text with a ", TextNode.text_type_text),
+            TextNode("italic block", TextNode.text_type_italic),
             TextNode(" word", TextNode.text_type_text)
         ]
         self.assertEqual(actual, expected)
