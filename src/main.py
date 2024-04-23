bd644124649270c23f2d68d60f094c43f3190706
Barry Hanna <barryahanna@gmail.com>
2024-04-23 12:03:58 +0100

    Copy static WIP

--- src/main.py
+++ src/main.py
@@ -1,4 +1,22 @@
-from textnode import TextNode
+import os
+import shutil
 
-tNode = TextNode("This is a text node", "bold", "https://boot.dev")
-print(tNode)
+OUTPUT_DIR = "public"
+
+
+def clean_output_dir():
+    print("Cleaning out dir...")
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    full_path = os.path.join(dir_path, "..", "public")
+    print(f"{full_path} exists. Cleaning now...")
+
+    # shutil.rmtree(os.path.)
+
+
+def copy_static_assets():
+    print("Copying static assets")
+
+
+if __name__ == "__main__":
+    clean_output_dir()
+    copy_static_assets()
